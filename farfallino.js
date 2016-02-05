357618c98e97f3cf03dccb0716d8e71a47a8d7f2
Flavio Li Volsi <xthunderx@gmail.com>
2016-02-05 20:55:24 +0100

    Update farfallino.js

--- farfallino.js
+++ farfallino.js
@@ -4,10 +4,7 @@ module.exports = {
 	transform: function(str) {
 		var transformed = '';
 		for (var i=0;i<str.length;i++) {
-			transformed += str[i];
-			if (symbols.is_vowel(str[i])) {
-				transformed += 'f'+str[i];
-			}
+			transformed += symbols.is_vowel(str[i]) ? 'f'+str[i] : str[i];
 		}
 		return transformed;
 	}
